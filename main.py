70514285043d2c1eb8abc5a6da3c5b4ec502eccd
JianiWuWSCW <wuj@wsc.school.nz>
2024-06-20 09:16:19 +1200

    change

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
 #functions
+def intro():
+    # Ask the user their name 
+    name=input("What's your name?")
+    # Greet the user and introduce the quiz
+    print("Welcome to the Forest World!",name)
 
+    print("You are a famous researcher of unearthed cultural relics.During an archaeology,
+    You and your archaeological team are infatuated and trapped in this forest. 
+    In the evening, you still can't find the way out. As night falls, heavy fog rises in the forest. 
+    You can't see each other clearly, 
+    and for To keep warm, 
+    everyone had to huddle together. When it got light, you planned to continue on your way, 
+    but when you got up the next day, you found that everyone was gone.
+    You will wake up in a forest")
 
 
 #main code
+
