eb245a0b6a5c614be0697690ea6dd91dee7c1c42
Annawscw <159732983+Annawscw@users.noreply.github.com>
2024-06-20 10:28:27 +1200

    senario1

--- main.py
+++ main.py
@@ -2,5 +2,37 @@
 
 def senario5 ():
     pass
+#senario5
+
+print("You hear bushes ruffleing and go to investigate it. You come across a monster it see's you what do you do?")
+yes = input("Do you want to fight?")
+if yes == "yes":
+    print("You fight the monster!")
+elif yes == yes:
+    print("You are still fighting the monster")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 #main code
+
