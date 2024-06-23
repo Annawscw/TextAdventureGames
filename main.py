249b6326f652353830f74454ba29dda4daa390dc
Annawscw <159732983+Annawscw@users.noreply.github.com>
2024-06-24 11:24:20 +1200

    added stuff to senario 6 and 5

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+
 #functions
 
 def senario5 ():
     pass
+
 #senario5
+
 
 print("You hear bushes ruffleing and go to investigate it. You come across a monster it see's you what do you do?")
 yes = input("Do you want to fight?")
@@ -10,29 +13,55 @@ if yes == "yes":
     print("You fight the monster!")
 elif yes == yes:
     print("You are still fighting the monster")
+#senario6
+print (".")
+print (".")
+print (".")
+print ("Congratulations you have defeted the monster!")
 
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#senario7
 
 
 
 
 #main code
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
