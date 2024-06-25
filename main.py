c102b9b6a11abe112db5b1b8b4a099f51e894265
Annawscw <159732983+Annawscw@users.noreply.github.com>
2024-06-25 15:08:41 +1200

    updata 25 jun

--- main.py
+++ main.py
@@ -1,35 +1,47 @@
 
 #functions
 
+ #senario5----------------------------
+
 def senario5 ():
     pass
-
-#senario5
-
-
+answer = ["yes","no"]
 print("You hear bushes ruffleing and go to investigate it. You come across a monster it see's you what do you do?")
-yes = input("Do you want to fight?")
-if yes == "yes":
-    print("You fight the monster!")
-elif yes == yes:
-    print("You are still fighting the monster")
-#senario6
-print (".")
-print (".")
-print (".")
-print ("Congratulations you have defeted the monster!")
+while answer:
+    yes = input("Do you want to fight the monster?")
+    if yes == "yes":
+        print("You fight the monster!")
+    else:
+        input("yes") 
+        
+
+
+
+ #senario6--------------------------------
+
+
+def senario6 ():
+
+   
+    print (".")
+    print (".")
+    print (".")
+    print ("Congratulations you have defeted the monster!")
+    yes = input ("do you want to leave the forest?")
+    if yes == "yes":
+        print("")
+    elif yes == yes:
+        print("yes")
+    print ("you have finnished forest ecxape game do you want to play again?")
+
+
+
+#senario7---------------------------------
 
 
 
 
-
-
-#senario7
-
-
-
-
-#main code
+#main code------------------------------
 
 
 
