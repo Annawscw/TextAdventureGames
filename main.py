d6cb5b77441c9de9d9cc7abea3f48f0b26420daf
JianiWuWSCW <wuj@wsc.school.nz>
2024-06-28 16:52:09 +1200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 #functions
+
+
+
+#main code
+#functions
 def intro():
     
 
@@ -27,27 +32,26 @@ def intro():
 
 
     #give the background
-    print("You are a very famous researcher of unearthed cultural relics.During an archaeology,you and your archaeological team are infatuated and trapped in this forest.In the evening, you guys still can't find the way out.As night falls, heavy fog rises in the forest. You can't see each other clearly, and for To keep warm, everyone had to huddle together. When it got light, you planned to continue on your way, but when you got up the next day, you found that everyone was gone.")
+
+    print("You are a very famous researcher of unearthed cultural relics.")
+    print("During an archaeology,you and your archaeological team are infatuated and trapped in this forest.")
+    print("In the evening, you guys still can't find the way out.As night falls, heavy fog rises in the forest.")
+    print("You can't see each other clearly, and for To keep warm, everyone had to huddle together.")
+    print("When it got light, you planned to continue on your way, but when you got up the next day, you found that everyone was gone.")
     print("When you wake up in the forest,you are into the Forest World!")
     print("Your companions have also entered different worlds, and it is up to each of them to decide what secrets are hidden in them? This will be undone by you! Your first task is to solve the problem and get out of this forest alive!")
 
-    print("Please click anywhere on the page to enter next scenario")
+    
 
 
 #functions
 def scenario1():
     
     print("Morning!Welcome to the Forest World!")
+    print("溫馨提示：拍拍你自己的臉讓自己保持清醒和警惕，森林中的危險無處不在，別因爲自己的大意之舉而喪命")
+    print("")
     print("You are the warrior chosen by the King of the Forest, and I am the messenger of this forest world, you cannot see me, but you can hear my voice, I will help you read the questions, and I will remind you of some of the more detailed details of the questions, so that you will not die so soon")
     print("Good luck!")
-
-    print("Ok.")
-    print("Now,you have two options:1,walk into the deeper forest(go walk front)2,get killed from the system.")
-    print("Please listen the question careful,give the system your best answer,thank you.")
-
-    input("Do want to walk front/into the deeper forest?")
-
-    if 
 
 #functions
 
@@ -57,5 +61,6 @@ def dieofstrvation():
 #main code
 
 intro()
-scenario1()
+
+
     
